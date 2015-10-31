3e8612587a56160809de60d13a29e841929fa732
PanchMahaBhut <bloodanzel@gmail.com>
2015-10-31 08:56:04 +0530

    Create MStyle.php

---
+++ MStyle/MStyle.php
@@ -0,0 +1,35 @@
+<?php namespace MStyle; //Namespace At Beginning
+
+/*@AUTHOR Manish Dhruw <BloodAnzel@gmail.com>
++=======================================================================================================================
+|Starting Comment Block
+|
+|
+|
+|
+|@CREATED -Fri 30 Oct 2015 08:33:34 PM IST 
+|@UPDATED
+| -
+|
+|
+|ENDING Comment Block
++---------------------------------------------------------------------------------------------------------------------*/
+
+class MStyle
+{
+
+	// Method should have brackets
+	// <= for Return
+	sayHello()
+	{
+		<= "Hello" ;
+	}
+	
+
+	//Inline 
+	inline() <= "This is Inline Function"; 
+
+
+	//Methods can be empty and can be ended with Semicolons without using curly brackets
+	emptyMethod();
+}
